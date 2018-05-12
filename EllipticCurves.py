9e39ace6e5c35d8fe75df18cefdf8a4134936afe
Davis Lister <davislister@Daviss-MacBook-Pro.local>
2018-05-12 14:43:55 -0400

    new changes

--- EllipticCurves.py
+++ EllipticCurves.py
@@ -11,6 +11,7 @@ def gcd(a,b):
   if(r==0):
     return b
   else:
+    print(a,r)
     return gcd(a,r)
 
 def sieve(n):
@@ -35,7 +36,7 @@ def pollard(n,b,attempts):
   a=2
   for j in range(2,2+attempts):
     x=(pow(a,m)-1)%n
-    d=gcd(x,n)
+    d=math.gcd(x,n)
     if (1<d) and (d<n):
       return d
   return 0
@@ -58,40 +59,51 @@ def elliptic_curve_addition_R(x1, y1, x2, y2, a, b):
 
 def elliptic_curve_addition_finite(x1, y1, x2, y2, a, b, n):
   slope = 0
-  if math.isnan(y1):
-    return y2
-  elif math.isnan(y2):
-    return y1
-  elif x1 == x2 and y1 == -y2:
-    return math.nan
+  if math.isnan(x1):
+    return x2, y2
+  elif math.isnan(x2):
+    return x1, y1
+  elif x1 == x2 and (y1 + y2) % n == 0:
+    return math.nan, math.nan
   elif x1 == x2 and y1 == y2:
     num = 3 * pow(x1, 2) + a
     denom = 2 * y1
-    if gcd(denom, n) != 1:
+    if math.gcd(denom, n) != 1:
         print("The chosen elliptic curve is not a group mod n.")
     else:
         base = num % n
-        while gcd(base, denom) == 1:
+        while math.gcd(base, denom) == 1:
             base += n
-        slope = base / denom
+        slope = (base / denom) % n
   else:
     num = (y1 - y2)
     denom = (x1 - x2)
-    slope = (num / denom) % n
+    while num < 0:
+      num += n
+    while denom < 0:
+      denom += n
+    if float(int(num / denom)) != num / denom:
+      base = num % n
+      while float(int(base / denom)) != base / denom:
+        base += n
+      slope = (base / denom) % n
+    else:
+      slope = (num / denom) % n
   x3 = (pow(slope, 2) - x1 - x2) % n
   y3 = (-slope * x3 - y1 + slope * x1) % n
   return x3, y3
 
+#issue returning to original point under O
 def elliptic_curve_multiplication_finite(x1, y1, m, a, b, n):
   if m == 2:
     return elliptic_curve_addition_finite(x1, y1, x1, y1, a, b, n)
   else:
     next = elliptic_curve_multiplication_finite(x1, y1, m-1, a, b, n)
-    return elliptic_curve_addition_finite(next[0], next[1], x1, y1, a, b, n)
+    return elliptic_curve_addition_finite(int(next[0]), int(next[1]), x1, y1, a, b, n)
 
 
 def lenstra(n, b):
   m = lcm_list(b)
   a = random.randint(0, math.sqrt(n))
 
-print(elliptic_curve_addition_finite(9, 16, 5, 1, 2, 2, 17))
+print(elliptic_curve_addition_finite(5, 1, math.nan, math.nan, 2, 2, 17))
