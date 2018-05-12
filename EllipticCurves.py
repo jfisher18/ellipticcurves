6bcee09575bffd3b7d74024bf0c1b48f8d34a537
Davis Lister <davislister@Daviss-MacBook-Pro.local>
2018-05-12 01:50:13 -0400

    new changes

--- EllipticCurves.py
+++ EllipticCurves.py
@@ -38,4 +38,19 @@ def pollard(n,b,attempts):
       return d
   return 0
 
-print(pollard(5917,5,1))
+def elliptic_curve_addition(x1, y1, x2, y2, a, b):
+  slope = 0
+  if math.isnan(y1):
+    return y2
+  elif math.isnan(y2):
+    return y1
+  elif x1 == x2 and y1 == -y2:
+    return math.nan
+  elif x1 == x2 and y1 == y2:
+    slope = (3 * pow(x1,2) + a) / (2 * y1)
+  else:
+    slope = (y1 - y2) / (x1 - x2)
+  x3 = pow(slope,2) - x1 - x2
+  y3 = -slope * x3 - y1 + slope * x1
+  return x3, y3
+
